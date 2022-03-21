d005a2b413b57c7bb67e7d9b1d64e2dfb7e69312
ajaybhayani <ajaybhayani741@gmail.com>
2022-03-21 11:04:33 +0530

    latest change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Home1Component } from './home1/home1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Home1Component
   ],
   imports: [
     BrowserModule,
